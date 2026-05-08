7ab2e8c00e3b0c8219f0a95cdf3890849d096c23
Ilya Kolpakov <ilya.kolpakov@gmail.com>
2026-05-08 16:15:41 +0200

    switch to unstable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "ilya's nixos configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
